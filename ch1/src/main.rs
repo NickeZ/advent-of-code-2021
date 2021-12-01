fb1594ae02d208d110805c14160d25eb301383d8
Niklas Dusenlund <niklas@dusenlund.se>
2021-12-01 09:48:49 +0100

    more functional style

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -5,15 +5,16 @@ fn main() {
     let mut input = File::open(std::env::args().nth(1).unwrap()).unwrap();
     let mut line = String::new();
     input.read_to_string(&mut line).unwrap();
-    let mut prev = 0;
-    let mut count_inc = 0;
-    for (i, num) in line.split_whitespace().enumerate() {
-        let num = num.parse().unwrap();
-        if i != 0 && num > prev {
-            count_inc += 1;
-        }
-        prev = num;
-
-    }
-    println!("{}", count_inc);
+    let (_, count) = line
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .enumerate()
+        .fold((0, 0), |(prev, count), (i, num)| {
+            if i != 0 && num > prev {
+                (num, count + 1)
+            } else {
+                (num, count)
+            }
+        });
+    println!("{}", count);
 }
