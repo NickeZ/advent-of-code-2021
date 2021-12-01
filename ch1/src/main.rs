8d51f777a1551dbe7a88d25be5b81da99136fb1b
Niklas Dusenlund <niklas@dusenlund.se>
2021-12-01 10:36:50 +0100

    tuple_windows/filter

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -6,30 +6,27 @@ fn main() {
     let mut input = File::open(std::env::args().nth(1).unwrap()).unwrap();
     let mut line = String::new();
     input.read_to_string(&mut line).unwrap();
-    let (_, count) = line
-        .split_whitespace()
-        .map(|s| s.parse::<usize>().unwrap())
-        .enumerate()
-        .fold((0, 0), |(prev, count), (i, num)| {
-            if i != 0 && num > prev {
-                (num, count + 1)
-            } else {
-                (num, count)
-            }
-        });
-    println!("{}", count);
 
-    let (_, count) = line
+    let count = line
         .split_whitespace()
         .map(|s| s.parse::<usize>().unwrap())
         .tuple_windows()
-        .enumerate()
-        .fold((0, 0), |(prev, count), (i, (a, b, c))| {
-            if i != 0 && (a + b + c) > prev {
-                ((a + b + c), count + 1)
+        .filter(|(a, b)| if b > a { true } else { false })
+        .count();
+    println!("{}", count);
+
+    let count = line
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .tuple_windows()
+        .tuple_windows()
+        .filter(|((a1, a2, a3), (b1, b2, b3))| {
+            if b1 + b2 + b3 > a1 + a2 + a3 {
+                true
             } else {
-                ((a + b + c), count)
+                false
             }
-        });
+        })
+        .count();
     println!("{}", count);
 }
