8b3ecfa8d4cdb1d9d8627f5f4429e3d57140a688
Niklas Dusenlund <niklas@dusenlund.se>
2021-12-09 13:56:53 +0100

    use hashset

--- ch8/src/main.rs
+++ ch8/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashSet;
 
 fn main() {
     let mut input = File::open(std::env::args().nth(1).unwrap()).unwrap();
@@ -28,14 +29,13 @@ fn main() {
     {
         let mut sum = 0u32;
         for line in content.trim().split("\n") {
-            let left = line.split("|").next().unwrap();
-            let right = line.split("|").nth(1).unwrap();
-
-            let integers = find_encoding(&left);
+            let left = line.split("|").next().unwrap().split_whitespace().map(|s| HashSet::from_iter(s.chars())).collect::<Vec<HashSet<_>>>();
+            let integers = find_encoding(left);
 
             let mut code = String::new();
-            for num in right.split_whitespace() {
-                let i = integers.iter().enumerate().find(|x| str_eq_set(x.1, num)).unwrap();
+            let right = line.split("|").nth(1).unwrap();
+            for num in right.split_whitespace().map(|s| HashSet::from_iter(s.chars())) {
+                let i = integers.iter().enumerate().find(|x| x.1 == &num).unwrap();
                 code.push_str(&i.0.to_string())
             }
             sum += str::parse::<u32>(&code).unwrap();
@@ -44,92 +44,60 @@ fn main() {
     }
 }
 
-fn find_encoding<'a>(left: &str) -> [String; 10] {
-    let left = left.split_whitespace();
-    let mut integers: [String; 10] = Default::default();
-    // find 1
-    for s in left.clone() {
+fn find_encoding<'a>(left: Vec<HashSet<char>>) -> [HashSet<char>; 10] {
+    let mut integers: [HashSet<char>; 10] = Default::default();
+    for s in &left {
+        // find 1
         if s.len() == 2 {
-            integers[1] = String::from(s);
-            break;
+            integers[1] = s.clone();
         }
-    }
-    // find 4
-    for s in left.clone() {
+        // find 4
         if s.len() == 4 {
-            integers[4] = String::from(s);
-            break;
+            integers[4] = s.clone();
         }
-    }
-    // find 7
-    for s in left.clone() {
+        // find 7
         if s.len() == 3 {
-            integers[7] = String::from(s);
-            break;
+            integers[7] = s.clone();
         }
-    }
-    // find 8
-    for s in left.clone() {
+        // find 8
         if s.len() == 7 {
-            integers[8] = String::from(s);
-            break;
+            integers[8] = s.clone();
         }
     }
-    // find 3
-    for s in left.clone() {
-        if s.len() == 5 && str_union(s, &integers[1]).len() == 2 {
-            integers[3] = String::from(s);
-            break;
+    for s in &left {
+        // find 3
+        if s.len() == 5 && s.intersection(&integers[1]).count() == 2 {
+            integers[3] = s.clone();
         }
-    }
-    // find 9
-    for s in left.clone() {
-        if s.len() == 6 && str_union(s, &integers[4]).len() == 4 {
-            integers[9] = String::from(s);
-            break;
+        // find 9
+        if s.len() == 6 && s.intersection(&integers[4]).count() == 4 {
+            integers[9] = s.clone();
         }
-    }
-    // find 0
-    for s in left.clone() {
-        if s.len() == 6 && str_union(s, &integers[1]).len() == 2 && str_union(s, &integers[4]).len() == 3 {
-            integers[0] = String::from(s);
-            break;
+        // find 0
+        if s.len() == 6 && s.intersection(&integers[1]).count() == 2 && s.intersection(&integers[4]).count() == 3 {
+            integers[0] = s.clone();
         }
     }
     // find 6
-    for s in left.clone() {
-        if s.len() == 6 && str_union(s, &integers[0]).len() == 5 && str_union(s, &integers[9]).len() == 5 {
-            integers[6] = String::from(s);
+    for s in &left {
+        if s.len() == 6 && s.intersection(&integers[0]).count() == 5 && s.intersection(&integers[9]).count() == 5 {
+            integers[6] = s.clone();
             break;
         }
     }
     // find 5
-    for s in left.clone() {
-        if s.len() == 5 && str_union(s, &integers[6]).len() == 5 {
-            integers[5] = String::from(s);
+    for s in &left {
+        if s.len() == 5 && s.intersection(&integers[6]).count() == 5 {
+            integers[5] = s.clone();
             break;
         }
     }
     // find 2
-    for s in left.clone() {
-        if s.len() == 5 && str_union(s, &integers[5]).len() == 3 {
-            integers[2] = String::from(s);
+    for s in &left {
+        if s.len() == 5 && s.intersection(&integers[5]).count() == 3 {
+            integers[2] = s.clone();
             break;
         }
     }
     integers
-}
-
-pub fn str_union(a: &str,b: &str) -> String {
-    let mut s = String::new();
-    for item in a.chars() {
-        if let Some(found) = b.chars().find(|x| *x ==item) {
-            s.push(found)
-        }
-    }
-    s
-}
-
-pub fn str_eq_set(a: &str, b: &str) -> bool {
-    a.len() == b.len() && str_union(a, b).len() == a.len()
 }
